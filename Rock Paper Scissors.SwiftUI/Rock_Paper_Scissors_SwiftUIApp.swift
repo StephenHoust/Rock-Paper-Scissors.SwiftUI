07ed15e56634d8892d9f55ae3a2803dc048f70e4
Stephen Houst <38192703+StephenHoust@users.noreply.github.com>
2022-10-24 11:43:08 -0700

    Initial Commit

---
+++ Rock Paper Scissors.SwiftUI/Rock_Paper_Scissors_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Scissors_SwiftUIApp.swift
+//  Rock Paper Scissors.SwiftUI
+//
+//  Created by Stephen Houst on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Scissors_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
