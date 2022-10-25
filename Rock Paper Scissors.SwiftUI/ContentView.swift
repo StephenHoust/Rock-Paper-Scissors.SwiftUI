fdc10018912d590dea5f96f44fe3747c988731c1
Stephen Houst <38192703+StephenHoust@users.noreply.github.com>
2022-10-25 12:23:26 -0700

    Add game logic, scoring and new game features

--- Rock Paper Scissors.SwiftUI/ContentView.swift
+++ Rock Paper Scissors.SwiftUI/ContentView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     enum gameMoves: String, CaseIterable, Identifiable {
-        case rock
-        case paper
-        case scissors
+        case rock, paper, scissors
         
         var id: gameMoves { self }
     }
@@ -21,11 +19,29 @@ struct ContentView: View {
     @State private var shouldWin = Bool.random()
     @State private var opponentMove = gameMoves.allCases.randomElement()!
     
-    var playerGoal: String {
-        if shouldWin {
-            return "WIN"
-        } else {
-            return "LOSE"
+    @State private var showingScore = false
+    @State private var gameOver = false
+    @State private var scoreTitle = ""
+    
+    var correctInput: gameMoves {
+        switch opponentMove {
+        case .rock:
+            switch shouldWin {
+            case true: return .paper
+            case false: return .scissors
+            }
+            
+        case .paper:
+            switch shouldWin {
+            case true: return .scissors
+            case false: return .rock
+            }
+            
+        case .scissors:
+            switch shouldWin {
+            case true: return .rock
+            case false: return .paper
+            }
         }
     }
     
@@ -37,18 +53,33 @@ struct ContentView: View {
         }
     }
     
-    func setupRound() -> Void {
-        opponentMove = gameMoves.allCases.randomElement()!
-        
-        if numberOfRounds == 0 {
-            shouldWin = Bool.random()
-        } else {
-            shouldWin.toggle()
-        }
+    func newGame() {
+        playerScore = 0
+        numberOfRounds = 0
+        setupRound()
     }
     
-    func selectMove(move: gameMoves) -> Void {
+    func setupRound() {
+        opponentMove = gameMoves.allCases.randomElement()!
+        shouldWin.toggle()
+    }
+    
+    func selectMove(playerMove: gameMoves) {
+        if playerMove == correctInput {
+            scoreTitle = "Correct!"
+            playerScore += 1
+        } else {
+            scoreTitle = "Incorrect!"
+            playerScore -= 1
+        }
         
+        numberOfRounds += 1
+        
+        if numberOfRounds >= 8 {
+            gameOver = true
+        } else {
+            showingScore = true
+        }
     }
     
     var body: some View {
@@ -58,11 +89,12 @@ struct ContentView: View {
             Text("Score: \(playerScore)")
                 .padding()
             Text("Opponent's Move: ") + Text(moveEmoji(move: opponentMove)).font(.largeTitle)
-            Text("Goal this round: \(playerGoal)")
-                .padding()
+            Text("Goal this round: \(shouldWin ? "WIN" : "LOSE")")
+                .padding(.bottom)
+            
             ForEach (gameMoves.allCases) { move in
                 Button(moveEmoji(move: move)) {
-                    selectMove(move: move)
+                    selectMove(playerMove: move)
                 }
                 .font(.largeTitle)
                 .padding(.vertical)
@@ -75,6 +107,19 @@ struct ContentView: View {
             Spacer()
         }
         .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: setupRound)
+        } message: {
+            Text(scoreTitle == "Correct!"
+                 ? "Good job!"
+                 : "The correct answer was \(moveEmoji(move: correctInput))."
+            )
+        }
+        .alert("Game over!", isPresented: $gameOver) {
+            Button("New Game", action: newGame)
+        } message: {
+            Text("\(scoreTitle) Your final score was \(playerScore)")
+        }
     }
 }
 
